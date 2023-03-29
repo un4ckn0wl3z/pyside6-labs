1a0df88770fb2199d39e43f1ff295bd6f0f68aef
Anuwat <admin@unknownclub.net>
2023-03-29 20:17:22 +0700

    feat: play with qtab

--- 005.widgets_tour/013.q_combo_box/main.py
+++ 005.widgets_tour/013.q_combo_box/main.py
@@ -1,9 +1,11 @@
 from PySide6.QtWidgets import QApplication
 from widget import Widget
 import sys
+from qt_material import apply_stylesheet
+
 
 app = QApplication(sys.argv)
-
+apply_stylesheet(app, theme='dark_teal.xml')
 widget = Widget()
 widget.show()
 
