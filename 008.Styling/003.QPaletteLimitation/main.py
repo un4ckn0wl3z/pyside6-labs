769d2e306a67a10548c47de761dec7884caa03d1
Anuwat <admin@unknownclub.net>
2023-04-09 12:23:20 +0700

    feat: play theme switching

--- 008.Styling/003.QPaletteLimitation/main.py
+++ 008.Styling/003.QPaletteLimitation/main.py
@@ -4,28 +4,28 @@ from PySide6.QtWidgets import QApplication, QLabel
 from widget import Widget
 from PySide6.QtGui import QPalette, QColor
 from PySide6.QtCore import Qt
-
+import qdarktheme
 app = QApplication(sys.argv)
 
-app.setStyle("Fusion")
+#app.setStyle("Fusion")
+qdarktheme.setup_theme(custom_colors={"primary": "#D0BCFF"})
+# darkPalette = app.palette() # [Credit] Palette code source : 
+#                             # https://gist.github.com/QuantumCD/6245215#file-qt-5-dark-fusion-palette
+# darkPalette.setColor(QPalette.Window, QColor(53,53,53))
+# darkPalette.setColor(QPalette.WindowText, Qt.white)
+# darkPalette.setColor(QPalette.Base, QColor(25,25,25))
+# darkPalette.setColor(QPalette.AlternateBase, QColor(53,53,53))
+# darkPalette.setColor(QPalette.ToolTipBase, Qt.white)
+# darkPalette.setColor(QPalette.ToolTipText, Qt.white)
+# darkPalette.setColor(QPalette.Text, Qt.white)
+# darkPalette.setColor(QPalette.Button, QColor(53,53,53))
+# darkPalette.setColor(QPalette.ButtonText, Qt.white)
+# darkPalette.setColor(QPalette.BrightText, Qt.red)
+# darkPalette.setColor(QPalette.Link, QColor(42, 130, 218))
+# darkPalette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+# darkPalette.setColor(QPalette.HighlightedText, Qt.black)
 
-darkPalette = app.palette() # [Credit] Palette code source : 
-                            # https://gist.github.com/QuantumCD/6245215#file-qt-5-dark-fusion-palette
-darkPalette.setColor(QPalette.Window, QColor(53,53,53))
-darkPalette.setColor(QPalette.WindowText, Qt.white)
-darkPalette.setColor(QPalette.Base, QColor(25,25,25))
-darkPalette.setColor(QPalette.AlternateBase, QColor(53,53,53))
-darkPalette.setColor(QPalette.ToolTipBase, Qt.white)
-darkPalette.setColor(QPalette.ToolTipText, Qt.white)
-darkPalette.setColor(QPalette.Text, Qt.white)
-darkPalette.setColor(QPalette.Button, QColor(53,53,53))
-darkPalette.setColor(QPalette.ButtonText, Qt.white)
-darkPalette.setColor(QPalette.BrightText, Qt.red)
-darkPalette.setColor(QPalette.Link, QColor(42, 130, 218))
-darkPalette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-darkPalette.setColor(QPalette.HighlightedText, Qt.black)
-
-app.setPalette(darkPalette)
+# app.setPalette(darkPalette)
 
 window = Widget()
 window.show()
--- 008.Styling/003.QPaletteLimitation/widget.py
+++ 008.Styling/003.QPaletteLimitation/widget.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QWidget,QInputDialog,QLineEdit
 from PySide6.QtGui import QPalette, QColor
 from PySide6.QtCore import Qt
 from designer.ui_widget import Ui_Widget
+import qdarktheme
 
 
 class Widget(QWidget, Ui_Widget):
@@ -9,3 +10,13 @@ class Widget(QWidget, Ui_Widget):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("QPalette Demo")
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.button_clicked)
+    
+    
+    def button_clicked(self, checked):
+        print("Clicked")
+        if checked:
+            qdarktheme.setup_theme("light")
+        else:
+            qdarktheme.setup_theme()
